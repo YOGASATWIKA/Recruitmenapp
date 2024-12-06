3702d8f2d629ef53a43ec2c8864073569fb66f80
Yoga satwika <satwikayoga3@gmail.com>
2024-12-06 19:49:08 +0800

    first commit

---
+++ src/main/java/com/javatechie/recruitmenapp/RecruitmenappApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.recruitmenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmenappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmenappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatechie/recruitmenapp/RecruitmenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.recruitmenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmenappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
